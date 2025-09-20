b744e83db8c6c863227ff0ff012c847e3fae5a15
Kidus Haileleul <kidus.haileleul@gmail.com>
2025-09-20 21:57:47 +0300

    Time: 0 ms (100.00%) | Memory: 17.6 MB (88.75%) - LeetSync

--- 861-flipping-an-image/flipping-an-image.py
+++ 861-flipping-an-image/flipping-an-image.py
@@ -3,10 +3,7 @@ class Solution:
         for i in range(len(image)):
             image[i]=image[i][::-1]
             for j in range(len(image[i])):
-                if image[i][j]==0:
-                    image[i][j]=1
-                else:
-                    image[i][j]=0
+                image[i][j]=1-image[i][j]
         return image
 
 
