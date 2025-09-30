75576a1e577893a85b5b417094118c375640d0e3
Kidus Haileleul <kidus.haileleul@gmail.com>
2025-09-30 22:07:01 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (10.82%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.py
+++ 782-jewels-and-stones/jewels-and-stones.py
@@ -1,8 +1,6 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        x=Counter(stones)
-        y=0
-        for i in range(len(jewels)):
-            if jewels[i] in stones:
-                y+=x[jewels[i]]
-        return y
+        x=0
+        for j in jewels:
+            x+=stones.count(j)
+        return x
